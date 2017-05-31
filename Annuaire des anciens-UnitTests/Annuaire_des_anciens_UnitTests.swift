9238bb16b553c791f210c486ece2b23ab9c273b6
Cizarius <student@MacBook-Air-de-Service.local>
2017-05-31 11:38:36 +0200

    add second unit test

--- Annuaire des anciens-UnitTests/Annuaire_des_anciens_UnitTests.swift
+++ Annuaire des anciens-UnitTests/Annuaire_des_anciens_UnitTests.swift
@@ -8,11 +8,16 @@
 
 import XCTest
 @testable import Annuaire_des_anciens
+import SwiftyJSON
 
 class Annuaire_des_anciens_UnitTests: XCTestCase {
 	
-	let myJson = "{\"id\":1,\"nom\":\"testnom\",\"prenom\":\"testprenom\",\"email\":\"test@test.com\",\"telephone_mobile\":\"0513513512\",\"telephone_fixe\":\"3058725393\",\"site_web\":\"siteTest.com\",\"adresse\":\"1 rue du Test\",\"code_postal\":\"54646\",\"ville\":\"TESTCITY\"}"
-    
+	let myJson: String = "{\"id\":1,\"nom\":\"testnom\",\"prenom\":\"testprenom\",\"email\":\"test@test.com\",\"telephone_mobile\":\"0513513512\",\"telephone_fixe\":\"3058725393\",\"site_web\":\"siteTest.com\",\"adresse\":\"1 rue du Test\",\"code_postal\":\"54646\",\"ville\":\"TESTCITY\"}"
+	
+	let emptyJson: JSON = "{}"
+	
+	let valid = NSJSONSerialization.isValidJSONObject(jsonObject)
+	
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -23,9 +28,14 @@ class Annuaire_des_anciens_UnitTests: XCTestCase {
         super.tearDown()
     }
 	
+	func transformToJson(mydata: String)->(JSON){
+		mydata.data(using: .utf8)
+		return JSON(data: mydata)
+	}
+	
 	func testParsing(){
-		
-		let myEleve = Eleve.init(myJson)
+		let js = transformToJson(mydata: myJson)
+		let myEleve = Eleve.init(js)
 		
 		XCTAssertEqual(myEleve.nom, "testnom")
 		XCTAssertEqual(myEleve.prenom, "testprenom")
@@ -37,7 +47,25 @@ class Annuaire_des_anciens_UnitTests: XCTestCase {
 		XCTAssertEqual(myEleve.code_postal, "54646")
 		XCTAssertEqual(myEleve.ville, "TESTCITY")
 	}
-    
+	
+	func testParsingToFail(){
+		
+		let js = transformToJson(mydata: emptyJson)
+		let myEleve = Eleve.init(js)
+		
+
+		XCTAssertEqual(myEleve.nom, "")
+		XCTAssertEqual(myEleve.prenom, "")
+		XCTAssertEqual(myEleve.email, "")
+		XCTAssertEqual(myEleve.telephone_mobile, "")
+		XCTAssertEqual(myEleve.telephone_fixe, "")
+		XCTAssertEqual(myEleve.site_web, "")
+		XCTAssertEqual(myEleve.adresse, "")
+		XCTAssertEqual(myEleve.code_postal, "")
+		XCTAssertEqual(myEleve.ville, "")
+		
+	}
+	
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
